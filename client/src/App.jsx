242fd47b039dda034f3ce0f671a2bd0055a64a7d
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-18 22:21:20 +0530

    create home page for quiz app

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Quiz from "./components/Quiz";
+import Result from "./components/Result";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/quiz", element: <Quiz /> },
+  { path: "/result", element: <Result /> },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
