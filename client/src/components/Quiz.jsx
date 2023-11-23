524f2d1eb7a6372ed8cb2b4727e9748da22a8ebf
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-23 11:41:25 +0530

    quiz section

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,5 +1,27 @@
+import Questions from "./Questions";
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const onNext = () => {
+    console.log("Click on next btn");
+  };
+
+  const onPrev = () => {
+    console.log("Click on prev btn");
+  };
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+      {/* display questions */}
+      <Questions />
+      <div className="grid">
+        <button className="btn prev" onClick={onPrev}>
+          Prev
+        </button>
+        <button className="btn next" onClick={onNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
